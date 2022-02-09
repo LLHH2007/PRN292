962b4e47824994ed159650ef19c2ecea2602a885
LongH3 <huynhlongploc@gmail.com>
2022-02-09 14:56:42 +0700

    commit2

--- Lab1/Lab1/Program.cs
+++ Lab1/Lab1/Program.cs
@@ -16,7 +16,7 @@
 using Microsoft.Win32;
 using System;
 using System.Collections.Generic;
-using System.Linq;
+using System.Linq
 using System.Runtime.CompilerServices;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
